995cd13fc984ffb52980dc0e3cc39bd8a7e683f9
Tyler Williams <tyler.williams@spothero.com>
2020-11-13 21:07:18 -0600

    Test branch (#6)
    * update test utils
    
    * Request Access to route
    
    * Create routes
    
    * accidently deleted content
    
    * simple navigation
    
    * Step 1: make initial request
    
    * Transaction History - uneditable
    
    * fix excluded files
    
    * test multiple actions
    
    * only pushes to master
    
    * added category unit test

--- src/components/CategoryList/AddCategoryForm/AddCategoryForm.spec.jsx
+++ src/components/CategoryList/AddCategoryForm/AddCategoryForm.spec.jsx
@@ -1,5 +1,40 @@
+import React from "react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
+
+import AddCategoryForm from "./AddCategoryForm";
+import { TestProvider } from "utils/TestUtils";
+
+const categories = [];
+
+const setup = (props) => {
+  const allProps = {
+    categories,
+    notify: jest.fn(),
+    addCategory: jest.fn(),
+    ...props,
+  };
+
+  return render(
+    <TestProvider>
+      <AddCategoryForm {...allProps} />
+    </TestProvider>
+  );
+};
+
 describe("<AddCategoryForm />", () => {
-  it("is a dummy test", () => {
-    expect(true).toBeTruthy();
+  let selectors;
+
+  beforeEach(() => {
+    selectors = setup();
+  });
+
+  it("mounts the fields when clicking the button", async () => {
+    const { queryByTestId, getByRole } = selectors;
+
+    fireEvent.click(getByRole("button", { name: "Add Category" }));
+
+    await waitFor(() => {
+      expect(queryByTestId("CategoryFormFields")).toBeTruthy();
+    });
   });
 });
