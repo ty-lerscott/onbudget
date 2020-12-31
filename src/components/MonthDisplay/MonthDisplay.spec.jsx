577604f8fb1d06796141436fe3a5c4e1869af5bf
Tyler Williams <tyler.williams@spothero.com>
2020-12-30 21:51:04 -0500

    Fix MonthDisplay test

--- src/components/MonthDisplay/MonthDisplay.spec.jsx
+++ src/components/MonthDisplay/MonthDisplay.spec.jsx
@@ -1,85 +1,101 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { userEvent, screen, advanceTo, clearDateMock } from '@tsw38/otis'
 
 import MonthDisplay from './MonthDisplay'
-import { TestProvider } from 'utils/test/utils'
 
-const setup = ({ stateProps, componentProps } = {}) => {
-	const props = {
-		setMonth: jest.fn(),
-		onPreviousClick: jest.fn(),
-		...componentProps
-	}
+import { renderWithStore, getState } from '__test__/utils'
 
-	return {
-		props,
-		selectors: render(
-			<TestProvider {...stateProps}>
-				<MonthDisplay {...props} />
-			</TestProvider>
-		)
-	}
+const setup = ({ store, props } = {}) => {
+  const componentProps = {
+    setMonth: jest.fn(),
+    onPreviousClick: jest.fn(),
+    ...props
+  }
+
+  return {
+    props: componentProps,
+    selectors: renderWithStore(<MonthDisplay {...componentProps} />, {
+      store: getState(
+        store || {
+          ui: {
+            date: new Date()
+          }
+        }
+      )
+    })
+  }
 }
 
 describe('<MonthDisplay />', () => {
-	it('renders nothing if there is no date', () => {
-		const {
-			selectors: { queryByTestId }
-		} = setup({
-			stateProps: {
-				state: {
-					ui: {
-						date: null
-					}
-				}
-			}
-		})
-		expect(queryByTestId('MonthDisplay')).toBeFalsy()
-	})
+  beforeEach(() => {
+    advanceTo(new Date(2020, 10, 5))
+  })
 
-	it('renders the month', () => {
-		const {
-			selectors: { getByTestId, getByText },
-			props
-		} = setup()
-		expect(getByTestId('MonthDisplay')).toBeTruthy()
-		expect(getByText('Oct 2020')).toBeTruthy()
-	})
+  afterEach(() => {
+    clearDateMock()
+  })
 
-	it('next button is unclickable by default', () => {
-		const {
-			selectors: { getByText }
-		} = setup()
+  it('renders nothing if there is no date', () => {
+    setup({
+      store: {
+        ui: {
+          date: null
+        }
+      }
+    })
 
-		expect(getByText('Next Month').closest('button')).toBeDisabled()
-		expect(getByText('Previous Month').closest('button')).not.toBeDisabled()
-	})
+    expect(screen.queryByTestId('MonthDisplay')).toBeNull()
+  })
 
-	it('changes to previous month when clicking button', () => {
-		const {
-			selectors: { getByText }
-		} = setup()
+  it('renders the month', () => {
+    setup({
+      store: {
+        ui: {
+          date: new Date()
+        }
+      }
+    })
 
-		const previousMonth = getByText('Previous Month').closest('button')
+    expect(screen.getByTestId('MonthDisplay')).toBeInTheDocument()
+    expect(screen.getByText('Nov 2020')).toBeInTheDocument()
+  })
 
-		fireEvent.click(previousMonth)
+  it('next button is unclickable by default', () => {
+    setup({
+      store: {
+        ui: {
+          date: new Date()
+        }
+      }
+    })
 
-		expect(getByText('Sep 2020')).toBeTruthy()
-	})
+    expect(screen.getByText('Next Month').closest('button')).toBeDisabled()
+    expect(
+      screen.getByText('Previous Month').closest('button')
+    ).not.toBeDisabled()
+  })
 
-	it('changes to next month after clicking previous month', () => {
-		const {
-			selectors: { getByText }
-		} = setup()
+  it('changes to previous month when clicking button', () => {
+    setup()
 
-		const previousMonth = getByText('Previous Month').closest('button')
-		const nextMonth = getByText('Next Month').closest('button')
+    const previousMonth = screen.getByText('Previous Month').closest('button')
 
-		fireEvent.click(previousMonth)
+    userEvent.click(previousMonth)
 
-		expect(getByText('Sep 2020')).toBeTruthy()
+    expect(screen.getByText('Oct 2020')).toBeInTheDocument()
+  })
 
-		fireEvent.click(nextMonth)
-		expect(getByText('Oct 2020')).toBeTruthy()
-	})
+  it('changes to next month after clicking previous month', () => {
+    setup()
+
+    const previousMonth = screen.getByText('Previous Month').closest('button')
+    const nextMonth = screen.getByText('Next Month').closest('button')
+
+    userEvent.click(previousMonth)
+
+    expect(screen.getByText('Oct 2020')).toBeInTheDocument()
+
+    userEvent.click(nextMonth)
+    expect(screen.getByText('Nov 2020')).toBeInTheDocument()
+  })
 })
